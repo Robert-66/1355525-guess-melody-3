048637f2b1b41689b7e69c9d5333282cbd22a5df
Robert Krepets <robert.krepets@megafon.ru>
2020-03-03 11:41:57 +0500

    Add state to the component App, describes WelcomeScreen display conditions and game screen display logic

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,26 +3,77 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import {GameType} from '../../const.js';
 import PropTypes from 'prop-types';
-
-const handleWelcomeButtonClick = () => {};
 
 class App extends React.PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      step: -1,
+    };
+
+    this.handleWelcomeButtonClick = this.handleWelcomeButtonClick.bind(this);
+    this.handleAnswerChange = this.handleAnswerChange.bind(this);
+  }
+
+  handleWelcomeButtonClick() {
+    this.setState({
+      step: 0,
+    });
+  }
+
+  handleAnswerChange() {
+    this.setState((prevState) => ({
+      step: prevState.step + 1,
+    }));
+  }
+
+  renderGameScreen() {
+    const {errorsCount, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errorsCount}
+          onClickWelcomeButton={this.handleWelcomeButtonClick}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={this.handleAnswerChange}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={this.handleAnswerChange}
+            />
+          );
+      }
+    }
+
+    return null;
   }
 
   render() {
-    const {errorsCount, questions} = this.props;
+    const {questions} = this.props;
 
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <WelcomeScreen
-              errorsCount={errorsCount}
-              onClickWelcomeButton={handleWelcomeButtonClick}
-            />
+            {this.renderGameScreen()}
           </Route>
           <Route exact path="/dev-artist">
             <ArtistQuestionScreen
