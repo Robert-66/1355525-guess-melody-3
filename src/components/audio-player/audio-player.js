50757e702574d62aeb7468c5b083e34da9c97159
Robert Krepets <robert.krepets@megafon.ru>
2020-03-09 08:58:46 +0500

    Add AudioPlayer component

---
+++ src/components/audio-player/audio-player.js
@@ -0,0 +1,96 @@
+import React, {createRef} from 'react';
+import PropTypes from 'prop-types';
+
+class AudioPlayer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      progress: 0,
+      isLoading: true,
+      isPlaying: this.props.isPlaying,
+    };
+
+    this.audioRef = createRef();
+    this.handlePlayPauseButtonClick = this.handlePlayPauseButtonClick.bind(this);
+  }
+
+  componentDidMount() {
+    const audio = this.audioRef.current;
+    audio.src = this.props.src;
+
+    audio.oncanplaythrough = () => this.setState({
+      isLoading: false,
+    });
+
+    audio.onplay = () => {
+      this.setState({
+        isPlaying: true,
+      });
+    };
+
+    audio.onpause = () => this.setState({
+      isPlaying: false,
+    });
+
+    audio.ontimeupdate = () => this.setState({
+      progress: audio.currentTime
+    });
+
+  }
+
+  componentDidUpdate() {
+    const audio = this.audioRef.current;
+
+    if (this.props.isPlaying) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  }
+
+  componentWillUnmount() {
+    const audio = this.audioRef.current;
+
+    audio.oncanplaythrough = null;
+    audio.onplay = null;
+    audio.onpause = null;
+    audio.ontimeupdate = null;
+    audio.src = ``;
+  }
+
+  handlePlayPauseButtonClick() {
+    this.setState((prevState) => ({
+      isPlaying: !prevState.isPlaying
+    }));
+    this.props.onClickPlayPauseButton();
+  }
+
+  render() {
+    const {isLoading, isPlaying} = this.state;
+
+    return (
+      <>
+        <button
+          className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+          type="button"
+          disabled={isLoading}
+          onClick={this.handlePlayPauseButtonClick}
+        />
+        <div className="track__status">
+          <audio
+            ref={this.audioRef}
+          />
+        </div>
+      </>
+    );
+  }
+}
+
+AudioPlayer.propTypes = {
+  onClickPlayPauseButton: PropTypes.func.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+};
+
+export default AudioPlayer;
